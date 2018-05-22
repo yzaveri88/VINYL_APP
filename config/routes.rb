5fd71e45bc0b628782c18d7d1b34d6d2b334544d
Alicia Eyb <aliciaeyb@gmail.com>
2018-05-22 15:48:30 +0200

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'records/index'
-  get 'records/show'
-  get 'records/new'
+  resources :records
+  # get 'records/index'
+  # get 'records/show'
+  # get 'records/new'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email:"yash@google.com", password:"123123")
+users = User.create(email: "yash@google.com", password: "123123", password_confirmation: "123123" )
+p users
+
 
 12.times do
 record = Record.create(user_id: 1, year: 1967,title:"Dark Side of the Moon",artist:"Pink Floyd",genre:"Rock",label:"Polydor" ,price:10)
